65656a7de58a6b8d64913a82ce9402d37577b03a
kr-aayushi <aayushi.s@knackroot.com>
2022-01-04 17:18:15 +0530

    basic of javascript

---
+++ helloworld.js
@@ -0,0 +1,65 @@
+/*document.write("hello");
+document.getElementById("th").innerHTML="this is a para";
+var a1=5;
+var a2=4;
+document.write(a1*a2);
+
+document.write("<br>",a2,"<br>");
+console.log("this is console");
+var age=prompt("Tell me your age");
+document.write("Your age is " , age);
+
+document.write("5+1=",5+1,"<br>");
+document.write("5-1=",5-1,"<br>");
+document.write("5*1=",5*1,"<br>");
+document.write("5/1=",5/1,"<br>");
+document.write("5%1=",5%1,"<br>");
+var foodcost=prompt("What is the share");
+document.write("total cost of food" , foodcost,"<br>");
+document.write("each one needs to give ", (foodcost/5).toFixed(2),"<br>");
+document.write("2^2 = ",Math.pow(2,2));*/
+
+/*var str1="codeme";
+document.write(str1.length,"<br>");
+document.write(str1.indexOf("me"),"<br>");
+document.write(str1.slice(2,6),"<br>");
+document.write(str1.replace("me","myself"),"<br>");
+
+var age=prompt("Tell me your age");
+if(age>20)
+{
+    document.write("school");
+}
+
+else 
+
+document.write("not possible");*/
+
+/*var age=prompt("Tell me your age");
+switch(age)
+{
+    case "20":document.write("20 yr........");
+    break;
+
+    case "21":document.write("21 yr........");
+    break;
+
+    case "23":document.write("23 yr........");
+    break;
+    case "22":document.write("22 yr........");
+    break;
+}*/
+var i=0;
+
+while(i<=100){
+document.write(i,"<br>");
+i++;
+}
+var b=["n","h","t"];
+document.write(b[1]);
+function a(){
+    document.write("rrrrrrrrrrr");
+    console.log("done");
+
+}
+a();
