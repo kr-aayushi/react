a1e4133670e90b7477d224a4af499c46ac7a7309
kr-aayushi <aayushi.s@knackroot.com>
2022-01-05 11:13:10 +0530

    constant in ES6

---
+++ constant_es6.js
@@ -0,0 +1,9 @@
+const pi=3.14;// declaration of constant variable globally 
+function circlearea(r){
+    const pi=10;
+    console.log("The area of circle is ", pi*r*r);
+
+
+}
+console.log(pi);
+circlearea(5);//uses pi=10 as it is declared locally 
