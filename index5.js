a5c44297bc66494a2ca4378cacf868520b0d63fc
kr-aayushi <aayushi.s@knackroot.com>
2022-01-05 10:36:05 +0530

    string

---
+++ index5.js
@@ -0,0 +1,6 @@
+
+var str1="codeme";
+console.log(str1.length,"<br>");
+console.log(str1.indexOf("me"),"<br>");
+console.log(str1.slice(2,6),"<br>");
+console.log(str1.replace("me","myself"),"<br>");
