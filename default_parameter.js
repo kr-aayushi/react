30c0980dd4ce9e319f0e151a0e36cda3b9daf79f
kr-aayushi <aayushi.s@knackroot.com>
2022-01-05 11:51:01 +0530

    default paramter 1

---
+++ default_parameter.js
@@ -0,0 +1,9 @@
+function add(num1=2,num2=3){
+    console.log(num1+num2);//will take default parameters 
+}
+add();
+
+function sub(num1=3,num2=3){
+    console.log(num1-num2);
+}
+sub(4,1);//uses external parameters
