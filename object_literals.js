b4099fa1af920ca51d6cbb835679678b7d954fa9
kr-aayushi <aayushi.s@knackroot.com>
2022-01-05 12:32:42 +0530

    literals

---
+++ object_literals.js
@@ -0,0 +1,14 @@
+var name="AAYUSHI";
+var age=22;
+var x;
+var ninja={
+    name,age,
+    chop(x){
+        console.log(`'I CHOPPED FOR ${x} TIMES'`);
+     
+}
+};
+console.log(ninja.chop(5));
+console.log(ninja.name);
+console.log(ninja.age);
+
