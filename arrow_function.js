f64948a99037c652ca6f5b6d1ce9f04906a232a6
kr-aayushi <aayushi.s@knackroot.com>
2022-01-05 12:59:55 +0530

    arrow function

---
+++ arrow_function.js
@@ -0,0 +1,17 @@
+var ninja=function()
+{    console.log("hiiii");
+}
+ninja();
+
+var ninja1=() =>        //simple arrow function
+{    console.log("hiiii,you");
+}
+ninja1();
+
+
+var ninja2=(name) =>
+{    console.log(...name,"hiiiiyou");
+}
+ninja2("AAYUSHI");
+
+
